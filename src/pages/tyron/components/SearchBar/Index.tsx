0aaf81432e8b43e88c5f47c1c1a85b6820041445
Ilham Bagas <ilhambagas@Ilhams-MacBook-Pro.local>
2022-10-03 13:54:03 +0700

    update searchbar resolver

--- src/pages/tyron/components/SearchBar/Index.tsx
+++ src/pages/tyron/components/SearchBar/Index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
   Text,
   StyleSheet,
@@ -9,103 +9,249 @@ import {
   Image,
   ActivityIndicator,
   Alert,
-} from 'react-native';
+} from "react-native";
 // import {useDispatch, useSelector} from 'react-redux';
-import {useTranslation} from 'react-i18next';
-import {updateDoc, updateDomain, updateName} from '../../redux/actions/user';
-import search from '../../assets/img/search.png';
-import * as tyron from '../../../../../node_modules/tyron';
-import { tyronThemeDark } from 'app/lib/controller/tyron/theme';
-import { userDoc, userDomain, userName } from 'app/lib/controller/tyron/user';
+import { useTranslation } from "react-i18next";
+import search from "../../assets/img/search.png";
+import * as tyron from "../../../../../node_modules/tyron";
+import { tyronThemeDark } from "app/lib/controller/tyron/theme";
+import {
+  userDoc,
+  userDomain,
+  userName,
+  userResolved,
+} from "app/lib/controller/tyron/user";
+import smartContract from "../../util/smartContract";
 
-const DeviceWidth = Dimensions.get('screen').width;
+const DeviceWidth = Dimensions.get("screen").width;
 
 export type Props = {
   navigation: any;
 };
 
-const SearchBar: React.FC<Props> = ({navigation}) => {
-  // const dispatch = useDispatch();
-  const {t} = useTranslation();
+const SearchBar: React.FC<Props> = ({ navigation }) => {
+  const zcrypto = tyron.Util.default.Zcrypto();
+  const { t } = useTranslation();
+  const { getSmartContract } = smartContract();
 
   const [loading, setLoading] = useState(false);
-  const [searchInput, setSearchInput] = useState('');
-  const isDark = tyronThemeDark.useValue()
+  const [searchInput, setSearchInput] = useState("");
+  const isDark = tyronThemeDark.useValue();
 
   const styles = isDark ? stylesDark : stylesLight;
-  const net = 'testnet';
+  const net = "testnet";
 
-  const resolveDid = () => {
-    let name = searchInput;
-    let domain = '';
-    if (searchInput.includes('@')) {
-      const [username = '', domain_ = ''] = searchInput.split('@');
-      name = username;
-      domain = domain_.replace('.did', '');
+  const resolveNft = async () => {
+    let name = searchInput.toLowerCase();
+    let domain = "";
+    if (searchInput.includes("@")) {
+      const [domain_ = "", username = ""] = searchInput.split("@");
+      name = username.toLowerCase().replace(".did", "");
+      domain = domain_
     }
     setLoading(true);
+    setSearchInput("");
     if (tyron.SearchBarUtil.default.isValidUsername(name)) {
-      tyron.SearchBarUtil.default
-        .fetchAddr(net, name, domain)
-        .then((address: string) => {
-          if (name === 'xpoints') {
-            setLoading(false);
-            navigation.navigate('XPoints');
+      await tyron.SearchBarUtil.default
+        .fetchAddr(net, name, "")
+        .then(async (addr) => {
+          if (
+            addr.toLowerCase() === "0x92ccd2d3b771e3526ebf27722194f76a26bc88a4"
+          ) {
+            throw new Error("premium");
           } else {
-            if (domain === '' || domain === 'did') {
-              tyron.SearchBarUtil.default
-                .Resolve(net, address)
-                .then((res: any) => {
-                  setSearchInput('');
-                  userDoc.set(res)
-                  userName.set(name)
-                  userDomain.set(domain)
-                  setLoading(false);
-                  navigation.navigate('Services');
-                })
-                .catch((err: any) => {
-                  setLoading(false);
-                  Alert.alert(JSON.stringify(err));
-                });
-            } else {
-              userName.set(name)
-              userDomain.set(domain)
-              setLoading(false);
-              setSearchInput('');
-              navigation.navigate('Stake');
+            return addr;
+          }
+        })
+        .then(async (addr) => {
+          let _addr = addr;
+          if (domain !== "") {
+            try {
+              _addr = await tyron.SearchBarUtil.default.fetchAddr(
+                net,
+                name,
+                domain
+              );
+            } catch (error) {
+              throw new Error("domNotR");
             }
           }
+
+          let res = await getSmartContract(_addr, "version");
+          const version = res.result.version.slice(0, 7);
+          userResolved.set({
+            name: name,
+            domain: domain,
+            addr: _addr,
+          });
+          userName.set(name);
+          userDomain.set(domain);
+          switch (version.toLowerCase()) {
+            case "didxwal":
+              resolveDid(name, "did");
+              break;
+            case "xwallet":
+              resolveDid(name, "did");
+              break;
+            case "initi--":
+              resolveDid(name, "did");
+              break;
+            case "xpoints":
+              navigation.navigate("XPoints");
+              setLoading(false);
+              break;
+            case "tokeni-":
+              // Router.push('/fungibletoken/nft')
+              setLoading(false);
+              break;
+            case "$siprox":
+              // Router.push('/ssidollar')
+              setLoading(false);
+              break;
+            default:
+              // It could be an older version of the DIDxWallet
+              resolveDid(name, domain);
+              break;
+          }
+        })
+        .catch(async (error) => {
+          if (String(error).slice(-7) === "premium") {
+            Alert.alert("Get in contact for more info.");
+          } else if (String(error).slice(-7) === "domNotR") {
+            Alert.alert("Unregistered DID Domain");
+          } else {
+            try {
+              await tyron.SearchBarUtil.default.fetchAddr(net, name, "");
+              Alert.alert("Upgrade required");
+              userName.set(name);
+              navigation.navigate("Services");
+            } catch (error) {
+              Alert.alert("Username available to buy");
+            }
+          }
+          setLoading(false);
         })
         .catch(() => {
           setLoading(false);
-          Alert.alert('Username available to buy');
+          Alert.alert("Username available to buy");
         });
     } else {
       setLoading(false);
-      Alert.alert('Invalid username');
+      Alert.alert("Invalid username");
     }
+  };
+
+  const resolveDid = async (_username: string, _domain: string) => {
+    await tyron.SearchBarUtil.default
+      .fetchAddr(net, _username, "did")
+      .then(async (addr) => {
+        await tyron.SearchBarUtil.default
+          .Resolve(net, addr)
+          .then(async (result: any) => {
+            const did_controller = zcrypto.toChecksumAddress(result.controller);
+            const res = await getSmartContract(addr, "version");
+            userDoc.set({
+              did: result.did,
+              controller: did_controller,
+              version: result.version,
+              doc: result.doc,
+              dkms: result.dkms,
+              guardians: result.guardians,
+            });
+
+            if (_domain === "did") {
+              userResolved.set({
+                name: _username,
+                domain: _domain,
+                addr: addr,
+                status: result.status,
+                version: res.result.version,
+              });
+              navigation.navigate("Services");
+            } else {
+              await tyron.SearchBarUtil.default
+                .fetchAddr(net, _username, _domain)
+                .then(async (domain_addr) => {
+                  const res = await getSmartContract(domain_addr, "version");
+                  userResolved.set({
+                    name: _username,
+                    domain: _domain,
+                    addr: domain_addr,
+                    status: result.status,
+                    version: res.result.version,
+                  });
+                  switch (res.result.version.slice(0, 8).toLowerCase()) {
+                    case "zilstake":
+                      navigation.navigate("Stake");
+                      break;
+                    case ".stake--":
+                      navigation.navigate("Stake");
+                      break;
+                    case "zilxwall":
+                      navigation.navigate("Stake");
+                      break;
+                    case "vcxwalle":
+                      navigation.navigate("SBT");
+                      break;
+                    case "sbtxwall":
+                      navigation.navigate("SBT");
+                      break;
+                    default:
+                      navigation.navigate("Servvices");
+                      setTimeout(() => {
+                        Alert.alert("Unregistered DID Domain.");
+                      }, 1000);
+                  }
+                })
+                .catch(() => {
+                  Alert.alert("Uninitialized DID Domain.");
+                  navigation.navigate("Servvices");
+                });
+            }
+            setTimeout(() => {
+              setLoading(false);
+            }, 1000);
+          })
+          .catch((err) => {
+            if (
+              String(err).includes("did_status") ||
+              String(err).includes(".result") ||
+              String(err).includes("null")
+            ) {
+              Alert.alert("Available in the future.");
+            } else {
+              Alert.alert(String(err));
+            }
+            setLoading(false);
+          });
+      })
+      .catch(() => {
+        Alert.alert("Upgrade required.");
+        navigation.navigate("Servvices");
+        setLoading(false);
+      });
   };
 
   return (
     <View>
-      <Text style={styles.text}>{t('SEARCH_NFT')}</Text>
+      <Text style={styles.text}>{t("SEARCH_NFT")}</Text>
       <View style={styles.bar} />
       <View style={styles.contentWrapper}>
         <TextInput
           style={styles.input}
           value={searchInput}
-          onChangeText={(text: string) => setSearchInput(text.toLowerCase())}
+          onChangeText={(text: string) => setSearchInput(text)}
         />
         <View style={styles.line} />
-        <TouchableOpacity onPress={resolveDid} style={styles.button}>
+        <TouchableOpacity onPress={resolveNft} style={styles.button}>
           <Image style={styles.searchIco} source={search} />
         </TouchableOpacity>
       </View>
       <View style={styles.bar} />
       {loading && (
         <ActivityIndicator
-          style={{marginTop: 50}}
-          color={isDark ? '#fff' : '#000'}
+          style={{ marginTop: 50 }}
+          color={isDark ? "#fff" : "#000"}
         />
       )}
     </View>
@@ -117,36 +263,36 @@ export default SearchBar;
 const stylesDark = StyleSheet.create({
   bar: {
     height: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginVertical: 10,
   },
   contentWrapper: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginVertical: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
     borderWidth: 2,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderRadius: 5,
   },
   text: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 14,
     marginTop: 10,
   },
   input: {
     height: 40,
     width: DeviceWidth - 110,
-    color: '#fff',
+    color: "#fff",
     marginLeft: -10,
     marginRight: 10,
   },
   button: {
     width: 30,
     height: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   searchIco: {
     width: 17,
@@ -155,7 +301,7 @@ const stylesDark = StyleSheet.create({
   line: {
     width: 2,
     height: 25,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginHorizontal: 10,
   },
 });
@@ -163,35 +309,35 @@ const stylesDark = StyleSheet.create({
 const stylesLight = StyleSheet.create({
   bar: {
     height: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginVertical: 10,
   },
   contentWrapper: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginVertical: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
     borderWidth: 2,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderRadius: 5,
   },
   text: {
-    color: '#000',
+    color: "#000",
     fontSize: 14,
     marginTop: 10,
   },
   input: {
     height: 40,
     width: DeviceWidth - 110,
-    color: '#000',
+    color: "#000",
     paddingHorizontal: 10,
   },
   button: {
     width: 30,
     height: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   searchIco: {
     width: 17,
@@ -200,7 +346,7 @@ const stylesLight = StyleSheet.create({
   line: {
     width: 2,
     height: 25,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginHorizontal: 10,
   },
 });
